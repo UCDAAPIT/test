ae40b0a71a2ffb6b0bf02623c795593928f84c3b
Andrew Smiley <andrew.smiley@live.com>
2014-11-03 17:18:06 -0500

    Added shell script

--- config_django.sh
+++ config_django.sh
@@ -4,6 +4,6 @@ cd /usr/share/nginx/www/
 git clone https://github.com/UCDAAPIT/test.git 
 cd test
 gunicorn tester.wsgi:application --bind=127.0.0.1:8001 &
-curl -0 https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
+curl -O https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
 service nginx restart
 
