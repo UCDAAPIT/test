203d790bc7b9c22fff290631a9596c52e68db1b6
Andrew Smiley <andrew.smiley@live.com>
2014-11-03 17:41:57 -0500

    shell tweak

--- config_django.sh
+++ config_django.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #script to automate the config of django+gunicorn+nginx
-cd /usr/share/nginx/www/
-git clone https://github.com/UCDAAPIT/test.git 
-cd test
+cd /usr/share/nginx/www/test2
 gunicorn tester.wsgi:application --bind=127.0.0.1:8001 &
+echo "run gunicorn" >> /log.txt
 curl -O https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
+echo "download config into nginx" >> /log.txt
 service nginx restart
 
