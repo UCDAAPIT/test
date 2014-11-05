cfe2edffa894e1a64520d981ca5b9c81ad92b9b0
Andrew Smiley <andrew.smiley@live.com>
2014-11-05 17:13:15 -0500

    Updates

--- config_django.sh
+++ config_django.sh
@@ -5,7 +5,7 @@ pip install django gunicorn
 echo "This is a test" > /log.txt
 cd /usr/share/nginx/www/test
 ls >> /log.txt
-gunicorn tester.wsgi:application --bind=0.0.0.0:12761 
+gunicorn tester.wsgi:application --bind=0.0.0.0:80 
 echo "run gunicorn" >> /log.txt
 echo "download config into nginx" >> /log.txt
 
