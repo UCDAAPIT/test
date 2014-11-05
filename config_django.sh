004660b2e3b2917edbeff11c06102633aa8e940f
Andrew Smiley <andrew.smiley@live.com>
2014-11-05 16:35:36 -0500

    script changes

--- config_django.sh
+++ config_django.sh
@@ -5,9 +5,9 @@ pip install django gunicorn
 echo "This is a test" > /log.txt
 cd /usr/share/nginx/www/test
 ls >> /log.txt
-gunicorn tester.wsgi:application --bind=127.0.0.1:13167 &
-netstat -lpn | grep :8001 >> /log.txt
+gunicorn tester.wsgi:application --bind=127.0.0.1:13167 
+#netstat -lpn | grep :8001 >> /log.txt
 echo "run gunicorn" >> /log.txt
-curl -O https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
+curl -O https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-available/default
 echo "download config into nginx" >> /log.txt
 
