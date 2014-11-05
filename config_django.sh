8b66a9331b6f88d3fdadc2b60d679193b96e504a
Andrew Smiley <andrew.smiley@live.com>
2014-11-05 11:27:35 -0500

    nginx

--- config_django.sh
+++ config_django.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #script to automate the config of django+gunicorn+nginx
-cd /usr/share/nginx/www/test2
+touch /log.txt
+cd /usr/share/nginx/www/test
 gunicorn tester.wsgi:application --bind=127.0.0.1:8001 &
 echo "run gunicorn" >> /log.txt
 curl -O https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
 echo "download config into nginx" >> /log.txt
-service nginx restart
 
