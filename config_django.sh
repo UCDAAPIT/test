5def363c6830960eaa7eb0b142590b12bb5441e3
Andrew Smiley <andrew.smiley@live.com>
2014-11-07 11:27:40 -0500

    Update

--- config_django.sh
+++ config_django.sh
@@ -6,8 +6,9 @@ echo "This is a test" > /log.txt
 cd /usr/share/nginx/www/test
 #cp index.html  /usr/share/nginx/www/index.html
 ls >> /log.txt
-gunicorn tester.wsgi:application --bind=127.0.0.1:8001 & 
+#gunicorn tester.wsgi:application --bind=127.0.0.1:8001 & 
 echo "run gunicorn" >> /log.txt
 curl -0 https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
 echo "download config into nginx" >> /log.txt
+
 
