9e2cffe180851fd2c7980dccfa74e0d924629eba
Andrew Smiley <andrew.smiley@live.com>
2014-11-07 12:20:30 -0500

    Test

--- config_django.sh
+++ config_django.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #script to automate the config of django+gunicorn+nginx
 easy_install pip
-pip install django gunicorn
+pip install django gunicorn uwsgi
 echo "This is a test" > /log.txt
-cd /usr/share/nginx/www/test
+cd /usr/share/nginx/www/test2
 ls >> /log.txt
-/usr/local/bin/gunicorn --pythonpath=/usr/share/nginx/www/test/ tester.wsgi:application --bind=127.0.0.1:80
+uwsgi --http :80 --wsgi-file tester/wsgi.py
 echo "run gunicorn" >> /log.txt
 #curl -0 https://raw.githubusercontent.com/UCDAAPIT/test/master/default > /etc/nginx/sites-enabled/default
 echo "download config into nginx" >> /log.txt
